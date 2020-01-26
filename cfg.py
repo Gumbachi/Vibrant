01db8b07f0324c61d2bde86a668a5027bc5df783
Jared Remsberg <jaredremsberg@gmail.com>
2020-01-26 02:51:10 -0500

    Comments on cfg

--- cfg.py
+++ cfg.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 client = MongoClient(f"mongodb+srv://Gumbachi:{os.environ['MONGO_PASS']}@cluster0-wglq9.mongodb.net/test?retryWrites=true&w=majority")
 db = client.VibrantDB
 
-
+# Choose collection in database
 if os.environ["BOT_VERSION"] == "real":
 	print("Using Real Collection")
 	coll = db.VibrantData
